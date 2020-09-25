3184bb78ad08f342f70b9d36333b1984e079ee2a
joannafawl <joanna.fawl@madetech.com>
2020-09-25 10:16:59 +0100

    pass tests to return array if integer is composite

--- lib/prime_factors.rb
+++ lib/prime_factors.rb
@@ -1,5 +1,21 @@
 class PrimeFactors
     def calculate_factors(integer)
-        integer
+        result = []
+        if integer == 1
+            result << 1
+        end
+        if integer % 2 == 0 
+            result << 2
+            integer /= 2
+        end
+        if integer % 2 == 0 
+            result << 2
+            integer /= 2
+        end
+        if integer % 3 == 0
+            result << 3
+            integer /= 3
+        end
+        result
     end
 end
--- spec/prime_factors_spec.rb
+++ spec/prime_factors_spec.rb
@@ -3,19 +3,31 @@ require_relative '../lib/prime_factors'
 RSpec.describe PrimeFactors do
     context "given an integer" do
         it "returns an integer" do
-            expect(PrimeFactors.new.calculate_factors(1)).to eq(1)
+            expect(PrimeFactors.new.calculate_factors(1)).to eq([1])
         end
     end
 
     context "given an integer" do
         it "returns an integer" do
-            expect(PrimeFactors.new.calculate_factors(2)).to eq(2)
+            expect(PrimeFactors.new.calculate_factors(2)).to eq([2])
         end
     end
 
     context "given an integer" do
         it "returns an integer" do
-            expect(PrimeFactors.new.calculate_factors(3)).to eq(3)
+            expect(PrimeFactors.new.calculate_factors(3)).to eq([3])
+        end
+    end
+
+    context "given an integer that is composite" do
+        it "returns an array" do
+            expect(PrimeFactors.new.calculate_factors(4)).to eq([2, 2])
+        end
+    end
+
+    context "given an integer that is composite" do
+        it "returns an array" do
+            expect(PrimeFactors.new.calculate_factors(6)).to eq([2, 3])
         end
     end
 end
